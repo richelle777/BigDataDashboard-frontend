4b9548484d9b5af43fda996d87d647c14a58c264
richelle777 <richellemayap@gmail.com>
2023-12-10 16:52:31 +0100

    base version of the project

---
+++ src/app/theme/layout/admin/navigation/navigation.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+
+export interface NavigationItem {
+  id: string;
+  title: string;
+  type: 'item' | 'collapse' | 'group';
+  icon?: string;
+  url?: string;
+  classes?: string;
+  external?: boolean;
+  target?: boolean;
+  breadcrumbs?: boolean;
+  children?: Navigation[];
+}
+
+export interface Navigation extends NavigationItem {
+  children?: NavigationItem[];
+}
+const NavigationItems = [
+  {
+    id: 'dashboard',
+    title: 'Dashboard',
+    type: 'group',
+    icon: 'icon-navigation',
+    children: [
+      {
+        id: 'default',
+        title: 'Dashboard',
+        type: 'item',
+        classes: 'nav-item',
+        url: '/dashboard',
+        icon: 'ti ti-dashboard',
+        breadcrumbs: false
+      }
+    ]
+  }
+];
+
+@Injectable()
+export class NavigationItem {
+  get() {
+    return NavigationItems;
+  }
+}
